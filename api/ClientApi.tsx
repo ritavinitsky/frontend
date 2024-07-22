fac4111b020d7cc72aa7c40769ff1e3b28502124
ritavinitsky <rita.vinitsky@gmail.com>
2024-07-22 14:14:46 +0300

    client v5

--- api/ClientApi.tsx
+++ api/ClientApi.tsx
@@ -1,11 +1,8 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-<<<<<<< HEAD
- baseURL: 'http://localhost:3000',
-=======
+
  baseURL: 'http://172.20.10.2:3000',
->>>>>>> f31084f (client v5)
  headers: { Accept: 'application/vnd.github.v3+json'},
 })
 
