1b56ac4725aafd279e8c1bafcc7fabecd634c1f6
rita vinitsky <rita.vinitsky@gmail.com>
2024-08-25 23:57:14 +0200

    v14

--- Components/Proccess.jsx
+++ Components/Proccess.jsx
@@ -1,9 +1,69 @@
+// import React, { useEffect, useState } from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import { Calendar } from 'react-native-calendars';
+// import axios from 'axios';
+
+// const ProcessPage: FC<{ route: any, navigation: any }> = ({ navigation, route }) => {
+//   const [progressData, setProgressData] = useState([]);
+//   const [markedDates, setMarkedDates] = useState({});
+//   const [currentDay, setCurrentDay] = useState('');
+
+//   useEffect(() => {
+//     const fetchProgressData = async () => {
+//       try {
+//         const response = await axios.get('http://backend-69iy.onrender.com/prograss');
+//         const data = response.data;
+//         setProgressData(data);
+//         setCurrentDay(data[0].date.split('T')[0]);
+
+//         // Create an object for markedDates
+//         const newMarkedDates = data.reduce((acc, item) => {
+//           const date = item.date.split('T')[0];
+//           acc[date] = {
+//             selected: true,
+//             selectedColor: item.passed ? 'green' : 'red',
+//           };
+//           return acc;
+//         }, {});
+
+//         setMarkedDates(newMarkedDates);
+//       } catch (error) {
+//         console.error('Error fetching progress data:', error);
+//       }
+//     };
+
+//     fetchProgressData();
+//   }, []);
+
+//   return (
+//     <View style={styles.container}>
+//       <Calendar
+//         current={currentDay}
+//         minDate={'2023-01-01'}
+//         maxDate={'2025-12-31'}
+//         monthFormat={'MM yyyy'}
+//         markedDates={markedDates}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     padding: 10,
+//   },
+// });
+
+// export default ProcessPage;
+
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import axios from 'axios';
 
-const ProcessPage = () => {
+const ProcessPage: FC<{ route: any, navigation: any }> = ({ navigation, route }) => {
   const [progressData, setProgressData] = useState([]);
   const [markedDates, setMarkedDates] = useState({});
   const [currentDay, setCurrentDay] = useState('');
@@ -13,11 +73,26 @@ const ProcessPage = () => {
       try {
         const response = await axios.get('http://backend-69iy.onrender.com/prograss');
         const data = response.data;
-        setProgressData(data);
-        setCurrentDay(data[0].date.split('T')[0]);
+
+        // Filter the data based on the user ID
+        const userProgressData = data.filter(item => item.userId === route.params.user_id);
+
+        // Log the entire response data
+        console.log('Full API response:', data);
+
+           // Log the filtered user progress data
+      console.log('id', route.params.user_id);
+
+           // Log the filtered user progress data
+      console.log('User progress data:', userProgressData);
+
+        // Set the currentDay to the first date of the filtered data
+        if (userProgressData.length > 0) {
+          setCurrentDay(userProgressData[0].date.split('T')[0]);
+        }
 
         // Create an object for markedDates
-        const newMarkedDates = data.reduce((acc, item) => {
+        const newMarkedDates = userProgressData.reduce((acc, item) => {
           const date = item.date.split('T')[0];
           acc[date] = {
             selected: true,
@@ -26,6 +101,9 @@ const ProcessPage = () => {
           return acc;
         }, {});
 
+          // Log the marked dates object
+      console.log('Marked dates:', newMarkedDates);
+
         setMarkedDates(newMarkedDates);
       } catch (error) {
         console.error('Error fetching progress data:', error);
@@ -33,7 +111,7 @@ const ProcessPage = () => {
     };
 
     fetchProgressData();
-  }, []);
+  }, [route.params.user_id]);
 
   return (
     <View style={styles.container}>
